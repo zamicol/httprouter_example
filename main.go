604406092429d0be46e8655b3c302440f114839a
Zamicol <zamicol@gmail.com>
2020-12-06 01:31:33 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	router := httprouter.New()
+	// Serve all static files from the public directory.
+	// .well-known is for certbot and tls.
+	router.ServeFiles("/assets/*filepath", http.Dir("web/public/assets"))
+
+	// Top level files must be named explicitly. (No * matching)
+	router.GET("/security.txt", file("web/public/standards/security.txt"))
+	router.GET("/robots.txt", file("web/public/standards/robots.txt"))
+	router.GET("/sitemap.xml", file("web/public/standards/sitemap.xml"))
+	router.GET("/hall-of-fame", file("web/public/standards/hall-of-fame"))
+
+	err := http.ListenAndServe(":8080", router)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// Closure for static files.
+func file(file string) func(http.ResponseWriter, *http.Request, httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		http.ServeFile(w, r, file)
+	}
+}
